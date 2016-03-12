2308f1405a317bc55519c475f2712d9755d8c5ee
vmohapatra <vmohapatra@expedia.com>
2016-03-11 18:58:11 -0800

    Adding subcontent to main content list

--- static_resources/js/myscripts.js
+++ static_resources/js/myscripts.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
-    $(".progress h3").click(function(){
+    //Specify click behavior on individual sections Level 1 : List 1
+    $(".progress .hdr_l1").click(function(){
         var clickedElementId = $(this).attr('id');
+        console.log(clickedElementId);
 
         switch(clickedElementId) {
             case "progress_h1":
@@ -51,5 +53,47 @@ $(document).ready(function(){
                 //Do something
                 $(".div_sublist").css('display','none');
         }
+    });
+
+    //Level 1 - Levels
+    $(".progress .hdr_l1_l1").click(function() {
+        var clickedElementId = $(this).attr('id');
+        console.log(clickedElementId);
+
+        switch(clickedElementId) {
+            case "progress_h1_h1" : 
+                if($("#sublist1_1").css('display') == 'block') {
+                    $(".div_sublist_1").css('display','none');
+                    $("#sublist1_1").css('display','none');
+                }
+                else {
+                    $(".div_sublist_1").css('display','none');
+                    $("#sublist1_1").css('display','block');
+                }
+                break;
+            case "progress_h1_h2" : 
+                if($("#sublist1_2").css('display') == 'block') {
+                    $(".div_sublist_1").css('display','none');
+                    $("#sublist1_2").css('display','none');
+                }
+                else {
+                    $(".div_sublist_1").css('display','none');
+                    $("#sublist1_2").css('display','block');
+                }
+                break;
+            case "progress_h1_h3" :
+                if($("#sublist1_3").css('display') == 'block') {
+                    $(".div_sublist_1").css('display','none');
+                    $("#sublist1_3").css('display','none');
+                }
+                else {
+                    $(".div_sublist_1").css('display','none');
+                    $("#sublist1_3").css('display','block');
+                }
+                break;
+            default : 
+                $(".div_sublist_1").css('display','none');
+        }
+
     });
 });
